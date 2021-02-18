6521a0f2827bfae23eaf2d42351a8b9201af52cf
qthan1004 <qthanh10041994@gmail,com>
2021-02-18 14:28:02 +0700

    update grouppage

--- server/src/controllers/posts.controllers.js
+++ server/src/controllers/posts.controllers.js
@@ -7,7 +7,6 @@ module.exports.getPosts = async (req, res) => {
 
 module.exports.postPost = (req, res) => {
     const { author, content } = req.body
-
     if (!author || !content) {
         return res.json({
             isSuccess: false,
@@ -44,13 +43,50 @@ module.exports.postPost = (req, res) => {
     });
 }
 
+//POST POST IN GROUP
+
+module.exports.postgroupPost = (req, res) => {
+    const { author, content, groupID } = req.body
+    if (!author || !content || !groupID) {
+        return res.json({
+            isSuccess: false,
+            message: 'Thiếu thông tin được yêu cầu',
+        });
+    }
+    const newPost = new Post({
+        ...req.body,
+        reaction: {
+            like: 0,
+            smile: 0,
+            angry: 0,
+            love: 0,
+            suprise: 0
+        },
+        comments: []
+    })
+    newPost.save(function (err, doc) {
+        if (err) {
+            return res.json({
+                isSuccess: false,
+                message: 'Database error',
+            })
+        } else {
+            return res.json({
+                isSuccess: true,
+                message: 'Bài viết đã được tạo',
+                newPost: doc,
+            })
+        }
+    });
+}
+
+
 module.exports.updatePost = (req, res) => {
     if (!req.params.id) {
         return res.json({
             isSuccess: false, message: 'Thiếu thông tin yêu cầu'
         });
     }
-    console.log('req.body', req.body)
 
     Post.findByIdAndUpdate(req.params.id, req.body, function (err, doc) {
         if (err) {
--- server/src/routes/posts.routers.js
+++ server/src/routes/posts.routers.js
@@ -4,5 +4,7 @@ const postsController = require('../controllers/posts.controllers')
 
 router.get('/', postsController.getPosts)
 router.post('/', postsController.postPost)
+router.post('/:id', postsController.postgroupPost)
 router.put('/:id', postsController.updatePost)
+
 module.exports = router
